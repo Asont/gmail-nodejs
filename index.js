6f8ce2cef6b52deee35ad412ac33b253e057df4e
Asont <a.druzhko@gmail.com>
2023-03-22 22:21:56 +0300

    initial commit2

--- index.js
+++ index.js
@@ -4,55 +4,23 @@ const nodemailer = require("nodemailer");
 const port = process.env.PORT || 6060
 const cors = require('cors')
 const bodyParser = require('body-parser')
-/*let smtpTransport = require('nodemailer-smtp-transport');*/
 
-index.use(cors())
 index.use(bodyParser.urlencoded({extended: false}))
 index.use(bodyParser.json())
 
-/*let transporter = nodemailer.createTransport(smtpTransport({
-  service: 'gmail',
-  host: 'smtp.gmail.com',
-  auth: {
-    user: "hoinyjibp@gmail.com",
-    pass: "swcztrvmnwqrlhqk"
-  }
-}));*/
 
-index.get('/', (req, res)=>{
+
+const corsOptions = {
+    origin: 'https://asont.github.io/portfolio/',
+    optionsSuccessStatus: 200
+}
+
+index.get('/', cors(corsOptions),(req, res)=>{
     res.send("Server ok")
 })
-/*
-
-index.post('/send', (req, res) => {
-
-  const {name, phone, email, subject, message} = req.body
-
-  transporter.sendMail({
-        from: email,
-        to: "hoinyjibp@gmail.com",
-        subject: subject,
-        html: `<p>${message}</p><p>${phone}</p><p>${name}</p>`
-      }
-      , function (error, info) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });
-
-  res.send('OK')
-})
-
-index.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-*/
 
 
-index.post('/send', async (req, res) => {
+index.post('/send', cors(corsOptions), async (req, res) => {
 
     const {name, phone, email, subject, message} = req.body
 
