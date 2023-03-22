6fc677f0b895f931ddb0b78520ce9270fec9ee08
Asont <a.druzhko@gmail.com>
2023-03-22 22:42:39 +0300

    initial commit2

--- index.js
+++ index.js
@@ -9,8 +9,19 @@ index.use(bodyParser.urlencoded({extended: false}))
 index.use(bodyParser.json())
 
 
+const allowedOrigins = ['https://asont.github.io'];
 index.use(cors({
-    origin: 'https://asont.github.io'
+    origin: function(origin, callback){
+        // allow requests with no origin
+        // (like mobile apps or curl requests)
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+            const msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
 }));
 
 
